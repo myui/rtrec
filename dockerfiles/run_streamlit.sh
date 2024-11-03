897ccf22f94ed9141d9e58165096b1d3a845ab14
myui <myui@apache.org>
2024-11-03 23:29:46 +0900

    Fixed script location

--- dockerfiles/run_streamlit.sh
+++ dockerfiles/run_streamlit.sh
@@ -6,4 +6,4 @@ if [ -z "${OMDB_API_KEY}" ]; then
 fi
 
 docker run --platform=linux/amd64 -it --rm -p 8501:8501 --env OMDB_API_KEY -v /Users/myui/workspace/myui/rtrec:/home/td-user/rtrec myui/rtrec:0.1 \
-streamlit run rtrec/samples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0
+streamlit run rtrec/examples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0
