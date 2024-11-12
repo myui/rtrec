5369c5002e6f7e4a3915a6df0b5e66ddab774467
myui <myui@apache.org>
2024-11-12 16:02:14 +0900

    Revised to build rtrec everytime

--- dockerfiles/run_streamlit.sh
+++ dockerfiles/run_streamlit.sh
@@ -9,4 +9,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -p 8501:8501 --env OMDB_API_KEY -v ${PROJECT_DIR}:/home/td-user/rtrec myui/rtrec:${ARCH} \
-bash -c "cd rtrec && source ~/.cargo/env && ~/.rye/shims/rye sync && ~/.rye/shims/streamlit run examples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0"
+bash -c "cd rtrec && source ~/.cargo/env && ~/.rye/shims/rye sync && ~/.rye/shims/rye remove rtrec && export RUST_LOG=debug && ~/.rye/shims/streamlit run examples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0"
