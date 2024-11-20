1a5c7cbf3ea0d27f94ea26a871f6ed1a200a895b
myui <myui@apache.org>
2024-11-20 20:30:54 +0900

    Revised for implicit build and multi-platform build

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-ARCH=`uname -m`
-echo "Building docker image for ${ARCH}..."
+#ARCH=`uname -m`
+#echo "Building docker image for ${ARCH}..."
 
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
-cd ${PROJECT_DIR}
+cd ${PROJECT_DIR}/dockerfiles
 
-docker buildx build --platform=linux/${ARCH} -f ./dockerfiles/Dockerfile --load -t myui/rtrec:${ARCH} .
-echo "Docker image 'myui/rtrec:${ARCH}' built successfully!"
+docker buildx build --platform=linux/amd64,linux/arm64 -f ./Dockerfile .
+docker buildx build --load --platform linux/amd64 -t myui/rtrec:amd64 .
+docker buildx build --load --platform linux/arm64 -t myui/rtrec:arm64 .
+
+echo "Docker images 'myui/rtrec:arm64' and 'myui/rtrec:amd64' built successfully!"
--- dockerfiles/run_notebook_server.sh
+++ dockerfiles/run_notebook_server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ARCH=$(uname -m)
+#ARCH=amd64
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -p 8888:8888 -p 6006:6006 -v ${PROJECT_DIR}:/home/td-user/rtrec myui/rtrec:${ARCH} \
-bash -c "cd rtrec && source ~/.cargo/env && ~/.rye/shims/rye sync && ~/.rye/shims/rye remove rtrec && export RUST_LOG=debug && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
+bash -c "cd rtrec && source ~/.cargo/env && cargo update && cargo clean && ~/.rye/shims/rye sync && ~/.rye/shims/rye remove rtrec && export RUST_LOG=debug && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
