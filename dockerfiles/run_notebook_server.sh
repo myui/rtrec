d05c5822a0cd4bf45a48f389c1f44661e8550c66
myui <myui@apache.org>
2024-11-27 13:49:30 +0900

    Revised to use up-to-date rtrec

--- dockerfiles/run_notebook_server.sh
+++ dockerfiles/run_notebook_server.sh
@@ -5,4 +5,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -p 8888:8888 -p 6006:6006 -v ${PROJECT_DIR}:/home/td-user/rtrec myui/rtrec:${ARCH} \
-bash -c "cd rtrec && source ~/.cargo/env && cargo update && cargo clean && ~/.rye/shims/rye sync && export RUST_LOG=info && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
+bash -c "cd rtrec && source ~/.cargo/env && cargo update && cargo clean && ~/.rye/shims/rye sync && ~/.rye/shims/rye remove rtrec && export RUST_LOG=info && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
