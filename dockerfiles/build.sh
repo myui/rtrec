10cd97fd61f9f1f6b67202befb4315c391d45b28
myui <myui@apache.org>
2024-11-28 17:22:20 +0900

    Revised to clone github rtrec repo

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-#ARCH=`uname -m`
-#echo "Building docker image for ${ARCH}..."
+set -ev
+
+ARCH=`uname -m`
+echo "Building docker image for ${ARCH}..."
 
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 cd ${PROJECT_DIR}/dockerfiles
 
-docker buildx build --platform=linux/amd64,linux/arm64 -f ./Dockerfile .
-docker buildx build --load --platform linux/amd64 -t myui/rtrec:amd64 .
-docker buildx build --load --platform linux/arm64 -t myui/rtrec:arm64 .
+#docker buildx build --platform=linux/amd64,linux/arm64 -f ./Dockerfile .
+#docker buildx build --load --platform linux/amd64 -t myui/rtrec:amd64 .
+#docker buildx build --load --platform linux/arm64 -t myui/rtrec:arm64 .
+docker buildx build --platform=linux/${ARCH} -f ./Dockerfile .  --load -t myui/rtrec:${ARCH}
 
-echo "Docker images 'myui/rtrec:arm64' and 'myui/rtrec:amd64' built successfully!"
+echo "Docker images 'myui/rtrec:${ARCH}' built successfully!"
