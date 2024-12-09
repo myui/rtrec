0f05b027fe6146994cf55eae6cda5c66d3adc380
myui <myui@apache.org>
2024-12-09 19:03:20 +0900

    Fixed imports

--- rtrec/models/__init__.py
+++ rtrec/models/__init__.py
@@ -1,6 +1,4 @@
 from rtrec._lowlevel import SlimMSE as Fast_SLIM_MSE
 from .slim import SLIM_MSE
-from .bprslim import BPR_SLIM
-from .fm import FactorizationMachines
 
-__all__ = ["Fast_SLIM_MSE", "SLIM_MSE", "BPR_SLIM", "FactorizationMachines"]
+__all__ = ["Fast_SLIM_MSE", "SLIM_MSE"]
