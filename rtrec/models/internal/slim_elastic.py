63307a2e6f4d81bf99a8ad0b18489fd6e41760cd
myui <myui@apache.org>
2024-12-10 12:16:47 +0900

    Moved internal models

--- rtrec/models/internal/slim_elastic.py
+++ rtrec/models/internal/slim_elastic.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Optional
 import numpy as np
 from numpy.typing import ArrayLike
 import scipy.sparse as sp
-from sklearn.linear_model import ElasticNet, SGDRegressor
+from sklearn.linear_model import ElasticNet
 import warnings
 from sklearn.exceptions import ConvergenceWarning
 from tqdm import tqdm
@@ -15,6 +15,10 @@ class ColumnarView:
         ind = self.csr_matrix.copy()
         ind.data = np.arange(len(ind.data)) # store the original data indices
         self.col_view = ind.tocsc()
+
+    @property
+    def matrix(self) -> sp.csr_matrix:
+        return self.csr_matrix
 
     @property
     def shape(self) -> tuple:
@@ -52,6 +56,48 @@ class ColumnarView:
         """
         self.csr_matrix.data[self.col_view[:, j].data] = values
 
+class CSCMatrixWrapper:
+
+    def __init__(self, csc_matrix: sp.csc_matrix):
+        self.csc_matrix = csc_matrix
+
+    @property
+    def matrix(self) -> sp.csc_matrix:
+        return self.csc_matrix
+
+    @property
+    def shape(self) -> tuple:
+        return self.csc_matrix.shape
+    
+    def get_col(self, j: int) -> sp.spmatrix:
+        """
+        Return the j-th column of the matrix.
+
+        Parameters
+        ----------
+        j : int
+            The column index.
+
+        Returns
+        -------
+        scipy.sparse.spmatrix
+            The j-th column of the matrix.
+        """
+        return self.csc_matrix[:, j].copy()
+    
+    def set_col(self, j: int, values: ArrayLike) -> None:
+        """
+        Set the j-th column of the matrix to the given values.
+
+        Parameters
+        ----------
+        j : int
+            The column index.
+        values : ArrayLike
+            The new values for the column.
+        """
+        self.csc_matrix[:, j] = values
+
 class FeatureSelectionWrapper:
 
     def __init__(self, model: ElasticNet, n_neighbors: int = 30):
@@ -60,7 +106,7 @@ class FeatureSelectionWrapper:
         self.n_neighbors = n_neighbors
         self.coef_ = None
 
-    def fit(self, X: sp.csr_matrix, y: np.ndarray):
+    def fit(self, X: sp.spmatrix, y: np.ndarray):
          # Compute dot products between items and the target
         feature_scores = X.T.dot(y).flatten()
         # Select the top-k similar items to the target item by sorting the dot products
@@ -91,7 +137,6 @@ class SLIMElastic:
             l1_ratio (float): The ratio between L1 and L2 regularization.
             positive_only (bool): Whether to enforce positive coefficients.
         """
-        self.model_name = config.get("model_name", "cd")
         self.eta0 = config.get("eta0", 0.001) # Learning rate used only for SGD
         self.alpha = config.get("alpha", 0.1) # Regularization strength
         self.l1_ratio = config.get("l1_ratio", 0.1) # mostly for L2 regularization for SLIM
@@ -105,52 +150,38 @@ class SLIMElastic:
         self.item_similarity = None
 
     def get_model(self):
-        if self.model_name == "cd" or self.model_name == "coordinate_descent":
-            return ElasticNet(
-                    alpha=self.alpha, # Regularization strength
-                    l1_ratio=self.l1_ratio,
-                    positive=self.positive_only, # Enforce positive coefficients
-                    fit_intercept=False,
-                    copy_X=False, # Avoid copying the input matrix
-                    precompute=True, # Precompute Gram matrix for faster computation
-                    max_iter=self.max_iter,
-                    tol=self.tol,
-                    selection='random', # Randomize the order of features
-                    random_state=self.random_state,
-                )
-        elif self.model_name == "sgd":
-            return SGDRegressor(
-                    penalty='elasticnet',
-                    eta0 = self.eta0,
-                    alpha=self.alpha,
-                    l1_ratio=self.l1_ratio,
-                    fit_intercept=False,
-                    max_iter=self.max_iter,
-                    tol=self.tol,
-                    random_state=self.random_state,
-                )
-        else:
-            raise ValueError(f"Invalid model name: {self.model_name}")
+        model = ElasticNet(
+                alpha=self.alpha, # Regularization strength
+                l1_ratio=self.l1_ratio,
+                positive=self.positive_only, # Enforce positive coefficients
+                fit_intercept=False,
+                copy_X=False, # Avoid copying the input matrix
+                precompute=True, # Precompute Gram matrix for faster computation
+                max_iter=self.max_iter,
+                tol=self.tol,
+                selection='random', # Randomize the order of features
+                random_state=self.random_state,
+            )
+        if self.nn_feature_selection is not None:
+            model = FeatureSelectionWrapper(model, n_neighbors=int(self.nn_feature_selection))
+        return model
 
-    def fit(self, interaction_matrix: sp.csr_matrix):
+    def fit(self, interaction_matrix: sp.coo_matrix):
         """
         Fit the SLIMElastic model to the interaction matrix.
 
         Args:
             interaction_matrix (csr_matrix): User-item interaction matrix (sparse).
         """
-        if not isinstance(interaction_matrix, sp.csr_matrix):
+        if not isinstance(interaction_matrix, sp.coo_matrix):
             raise ValueError("Interaction matrix must be a scipy.sparse.csr_matrix of user-item interactions.")
 
-        X = ColumnarView(interaction_matrix)
+        X = CSCMatrixWrapper(interaction_matrix)
         num_items = X.shape[1]
 
         self.item_similarity = np.zeros((num_items, num_items))  # Initialize similarity matrix
         
         model = self.get_model()
-
-        if self.nn_feature_selection is not None:
-            model = FeatureSelectionWrapper(model, n_neighbors=int(self.nn_feature_selection))
 
         # Ignore convergence warnings for ElasticNet
         with warnings.catch_warnings():
@@ -165,7 +196,7 @@ class SLIMElastic:
                 X.set_col(j, np.zeros_like(y.data))
 
                 # Fit the model
-                model.fit(X.csr_matrix, y.toarray().ravel())
+                model.fit(X.matrix, y.toarray().ravel())
 
                 # Update the item similarity matrix with new coefficients (weights for each user-item interaction)
                 self.item_similarity[:, j] = model.coef_
@@ -173,52 +204,53 @@ class SLIMElastic:
                 # Reattach the item column after training
                 X.set_col(j, y.data)
 
-        return self
-
     def partial_fit(self, interaction_matrix: sp.csr_matrix, user_ids: List[int]):
         """
         Incrementally fit the SLIMElastic model with new or updated users.
 
         Args:
-            interaction_matrix (coo_matrix): user-item interaction matrix (sparse).
+            interaction_matrix (csr_matrix): user-item interaction matrix (sparse).
             user_ids (list): List of user indices that were updated.
         """        
         user_items = set()
         for user_id in user_ids:
             user_items.update(interaction_matrix[user_id].indices.tolist())
-        return self.partial_fit_items(interaction_matrix, list(user_items))
+        self.partial_fit_items(interaction_matrix, list(user_items))
 
-    def partial_fit_items(self, interaction_matrix: sp.csr_matrix, updated_items: List[int]):
+    def partial_fit_items(self, interaction_matrix: sp.csc_matrix | sp.csr_matrix, updated_items: List[int]):
         """
         Incrementally fit the SLIMElastic model with new or updated items.
 
         Args:
-            interaction_matrix (coo_matrix): user-item interaction matrix (sparse).
+            interaction_matrix (csc_matrix | csr_matrix): user-item interaction matrix (sparse).
             updated_items (list): List of item indices that were updated.
         """
-        if not isinstance(interaction_matrix, sp.csr_matrix):
-            raise ValueError("Interaction matrix must be a scipy.sparse.csr_matrix of user-item interactions.")
-
-        X = ColumnarView(interaction_matrix)
+        if isinstance(interaction_matrix, sp.csr_matrix):
+            X = ColumnarView(interaction_matrix)
+        elif isinstance(interaction_matrix, sp.csc_matrix):
+            X = CSCMatrixWrapper(interaction_matrix)
+        else:
+            raise ValueError("Interaction matrix must be a scipy.sparse.csr_matrix or scipy.sparse.csc_matrix.")
 
         model = self.get_model()
 
-        if self.nn_feature_selection is not None:
-            model = FeatureSelectionWrapper(model, n_neighbors=int(self.nn_feature_selection))
-
-        old_size = self.item_similarity.shape[1]
-        new_size = max(updated_items) + 1
-        if new_size > old_size:
-            # Expand both rows and columns symmetrically
-            expanded_similarity = np.zeros((new_size, new_size))
-            expanded_similarity[:old_size, :old_size] = self.item_similarity
-            self.item_similarity = expanded_similarity
+        if self.item_similarity is None:
+            self.item_similarity = np.zeros((X.shape[1], X.shape[1]))
+        else:
+            # ensure the item similarity matrix is large enough to accommodate the new items
+            old_size = self.item_similarity.shape[1]
+            new_size = max(updated_items) + 1
+            if new_size > old_size:
+                # Expand both rows and columns symmetrically
+                expanded_similarity = np.zeros((new_size, new_size))
+                expanded_similarity[:old_size, :old_size] = self.item_similarity
+                self.item_similarity = expanded_similarity
 
         # Iterate through the updated items and fit the model incrementally
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=ConvergenceWarning)
 
-            for j in tqdm(updated_items):
+            for j in tqdm(updated_items, desc="Fitting SLIMElastic"):
                 # Target column (current item)
                 y = X.get_col(j)
 
@@ -226,7 +258,7 @@ class SLIMElastic:
                 X.set_col(j, np.zeros_like(y.data))
 
                 # Fit the model for the updated item
-                model.fit(X.csr_matrix, y.toarray().ravel())
+                model.fit(X.matrix, y.toarray().ravel())
 
                 # Update the item similarity matrix with new coefficients (weights for each user-item interaction)
                 self.item_similarity[:, j] = model.coef_
@@ -254,6 +286,25 @@ class SLIMElastic:
         # and the item similarity matrix
         return interaction_matrix[user_id].dot(self.item_similarity)
 
+    def predict_selected(self, user_id: int, item_ids: List[int], interaction_matrix: sp.csr_matrix):
+        """
+        Compute the predicted scores for a specific user and a subset of items.
+
+        Args:
+            user_id (int): The user ID (row index in interaction_matrix).
+            item_ids (List[int]): List of item indices to compute the predicted scores for.
+            interaction_matrix (csr_matrix): User-item interaction matrix.
+
+        Returns:
+            numpy.ndarray: Predicted scores for the user and the selected items.
+        """
+        if self.item_similarity is None:
+            raise RuntimeError("Model must be fitted before calling predict_selected.")
+
+        # Compute the predicted scores for the selected items by performing dot product between the user interaction vector
+        # and the item similarity matrix
+        return interaction_matrix[user_id, item_ids].dot(self.item_similarity[item_ids])
+
     def predict_all(self, interaction_matrix: sp.csr_matrix):
         """
         Compute the predicted scores for all users and items.
@@ -271,6 +322,37 @@ class SLIMElastic:
         # and the item similarity matrix
         return interaction_matrix.dot(self.item_similarity)
 
+    def recommend(self, user_id: int, interaction_matrix: sp.csr_matrix, candidate_item_ids: Optional[List[int]]=None, top_k: int=10, exclude_seen: bool=True) -> List[int]:
+        """
+        Recommend top-K items for a given user.
+
+        Args:
+            user_id (int): ID of the user (row index in interaction_matrix).
+            interaction_matrix (csr_matrix): User-item interaction matrix (sparse).
+            candidate_item_ids (List[int]): List of candidate item indices to recommend from. If None, recommend from all items.
+            top_k (int): Number of recommendations to return.
+            exclude_seen (bool): Whether to exclude items the user has already interacted with.
+
+        Returns:
+            List of recommended item indices.
+        """
+        # Get predicted scores for all items for the given user
+        if candidate_item_ids is None:
+            user_scores = self.predict(user_id, interaction_matrix).ravel()            
+        else:
+            user_scores = self.predict_selected(user_id, candidate_item_ids, interaction_matrix).ravel()
+
+        # Exclude items that the user has already interacted with
+        if exclude_seen:
+            seen_items = interaction_matrix[user_id].indices
+            user_scores[seen_items] = -np.inf  # Exclude seen items by setting scores to -inf
+
+        # Get the top-K items by sorting the predicted scores in descending order
+        # [::-1] reverses the order to get the items with the highest scores first
+        top_items = np.argsort(user_scores)[-top_k:][::-1]
+
+        return top_items
+    
     def similar_items(self, item_id: int, top_k: int=10):
         """
         Get the top-K most similar items to a given item.
@@ -291,30 +373,3 @@ class SLIMElastic:
         # Get the top-K similar items by sorting the similarity scores in descending order
         similar_items = np.argsort(item_similarity)[-1:-1-top_k:-1]
         return similar_items
-
-    def recommend(self, user_id: int, interaction_matrix: sp.csr_matrix, top_k: int=10, exclude_seen: bool=True) -> List[int]:
-        """
-        Recommend top-K items for a given user.
-
-        Args:
-            user_id (int): ID of the user (row index in interaction_matrix).
-            interaction_matrix (csr_matrix): User-item interaction matrix (sparse).
-            top_k (int): Number of recommendations to return.
-            exclude_seen (bool): Whether to exclude items the user has already interacted with.
-
-        Returns:
-            List of recommended item indices.
-        """
-        # Get predicted scores for all items for the given user
-        user_scores = self.predict(user_id, interaction_matrix).ravel()
-
-        # Exclude items that the user has already interacted with
-        if exclude_seen:
-            seen_items = interaction_matrix[user_id].indices
-            user_scores[seen_items] = -np.inf  # Exclude seen items by setting scores to -inf
-
-        # Get the top-K items by sorting the predicted scores in descending order
-        # [::-1] reverses the order to get the items with the highest scores first
-        top_items = np.argsort(user_scores)[-top_k:][::-1]
-
-        return top_items
