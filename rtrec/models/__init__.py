ba608d9086aeedbeaa16a40953071061805165a1
myui <myui@apache.org>
2024-12-16 17:33:37 +0900

    Fixed import error

--- rtrec/models/__init__.py
+++ rtrec/models/__init__.py
@@ -1,4 +1,4 @@
-from rtrec._lowlevel import SlimMSE as Fast_SLIM_MSE
+#from rtrec._lowlevel import SlimMSE as Fast_SLIM_MSE
 from .slim import SLIM
 
-__all__ = ["Fast_SLIM_MSE", "SLIM"]
+__all__ = ["SLIM"]
