edc6f9d71d6d626507b2130c82cb104ecde2fc8c
myui <myui@apache.org>
2024-12-17 22:02:20 +0900

    Fixes for uv

--- dockerfiles/run_notebook_server.sh
+++ dockerfiles/run_notebook_server.sh
@@ -5,4 +5,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -p 8888:8888 -p 6006:6006 -v ${PROJECT_DIR}/notebooks:/home/td-user/rtrec/notebooks myui/rtrec:${ARCH} \
-bash -c "cd rtrec && git fetch && git pull && source ~/.cargo/env && source ~/.rye/env && rye remove rtrec && export RUST_LOG=info && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
+bash -c "cd rtrec && git fetch && git pull && source ~/.cargo/env && . ~/.local/bin/env && uv sync && export RUST_LOG=info && ~/.rye/shims/jupyter-lab --ip=0.0.0.0 --no-browser --notebook-dir=/home/td-user/rtrec/notebooks"
--- dockerfiles/run_shell.sh
+++ dockerfiles/run_shell.sh
@@ -4,4 +4,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -v ${PROJECT_DIR}:/home/td-user/rtrec myui/rtrec:${ARCH} \
-bash -c "cd rtrec && source ~/.cargo/env && ~/.rye/shims/rye sync && ~/.rye/shims/rye remove rtrec && export RUST_LOG=debug && bash"
+bash -c "cd rtrec && source ~/.cargo/env && . ~/.local/bin/env && uv sync & export RUST_LOG=debug && bash"
