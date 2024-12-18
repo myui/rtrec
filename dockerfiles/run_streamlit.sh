091ec4c77c6d82d8906cf907c72d14e6d71ccab9
myui <myui@apache.org>
2024-12-18 17:26:30 +0900

    Revised to for PYTHONPATH

--- dockerfiles/run_streamlit.sh
+++ dockerfiles/run_streamlit.sh
@@ -9,4 +9,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -p 8501:8501 --env OMDB_API_KEY -v ${PROJECT_DIR}/examples/streamlit:/home/td-user/rtrec/examples/streamlit myui/rtrec:${ARCH} \
-bash -c "cd rtrec && ~/.venv/bin/streamlit run examples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0"
+bash -c "cd rtrec && git fetch && git pull && . ~/.local/bin/env && uv sync && uv pip install streamlit && .venv/bin/streamlit run examples/streamlit/movielens_dashboard.py --server.port=8501 --server.address=0.0.0.0"
