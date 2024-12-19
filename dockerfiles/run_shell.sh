cace1f3a2974db1e3eff3d4fd003034271b7085e
myui <myui@apache.org>
2024-12-19 16:42:25 +0900

    cargo is unused

--- dockerfiles/run_shell.sh
+++ dockerfiles/run_shell.sh
@@ -4,4 +4,4 @@ ARCH=$(uname -m)
 PROJECT_DIR=$(cd $(dirname $0); cd ..; pwd)
 
 docker run --platform=linux/${ARCH} -it --rm -v ${PROJECT_DIR}:/home/td-user/rtrec myui/rtrec:${ARCH} \
-bash -c "cd rtrec && source ~/.cargo/env && . ~/.local/bin/env && uv sync & export RUST_LOG=debug && bash"
+bash -c "cd rtrec && . ~/.local/bin/env && uv sync & bash"
