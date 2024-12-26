9d328dc03b5ca59af254c7f5e0c7493a74598042
myui <myui@apache.org>
2024-12-26 11:30:41 +0900

    Add more tests

--- tests/models/test_slim.py
+++ tests/models/test_slim.py
@@ -39,11 +39,39 @@ def test_recommend_no_interactions(model):
     assert recommendations == []
 
 def test_similar_items_no_data(model):
-    interactions = [('user_1', 'item_1', 1622470427.0, 5.0)]
+    import time
+    current_unixtime = time.time()
+    interactions = [('user_1', 'item_1', 1622470427.0, 5.0), ('user_2', 'item_2', current_unixtime, -2.0)]
     model.fit(interactions)
+
+    def yield_interactions():
+        for interaction in interactions:
+            yield interaction
+
+    model.fit(yield_interactions())
+
     similar_items = model.similar_items('item_1', top_k=5)
     # Since there are no items, similar items should be empty
     assert similar_items == []
+
+def test_fit_and_recommend(model):
+    import time
+    current_unixtime = time.time()
+    interactions = [('user_1', 'item_1', current_unixtime, 5.0),
+                   ('user_2', 'item_2', current_unixtime, -2.0),
+                   ('user_2', 'item_1', current_unixtime, 3.0),
+                   ('user_2', 'item_4', current_unixtime, 3.0),
+                   ('user_1', 'item_3', current_unixtime, 4.0)]
+    model.fit(interactions)
+
+    def yield_interactions():
+        for interaction in interactions:
+            yield interaction
+    model.fit(yield_interactions())
+
+    recommendations = model.recommend('user_1', top_k=5)
+    # Verify that the recommendations are correct
+    assert recommendations == ["item_4", "item_2"]
 
 if __name__ == "__main__":
     pytest.main()
