4139632d299a6c5ecd25b9402d85178748c2a3a4
myui <myui@apache.org>
2025-04-25 09:51:59 +0900

    Added force_identify option

--- rtrec/utils/identifiers.py
+++ rtrec/utils/identifiers.py
@@ -1,24 +1,42 @@
 from typing import Any, Optional
 
+import numpy as np
+
 class IdentifierError(Exception):
     """Custom exception for Identifier class key errors."""
     def __init__(self, id_name: str, obj_id: int):
         super().__init__(f"Identifier not found for {id_name}: {obj_id}")
 
 class Identifier:
-    def __init__(self, name: str="ID") -> None:
+    def __init__(self, name: str="ID", force_identify: bool=False, **kwargs: Any) -> None:
+        """
+        Initialize the Identifier class.
+        Args:
+            name (str): Name of the identifier.
+            force_identify (bool): Flag to force identification.
+        """
         self.name = name
+        self.force_identify = force_identify  # Flag to force identification
         self.obj_to_id: dict[Any, int] = {}  # Store object-to-ID mapping
         self.id_to_obj: list[Any] = []  # Store ID-to-object mapping
-        self.pass_through : Optional[bool] = None # If True, return the object as-is if it is an integer
+        if force_identify:
+            self.pass_through = False
+        else:
+            self.pass_through : Optional[bool] = None # If True, return the object as-is if it is an integer
 
     def identify(self, obj: Any) -> int:
         # If the object is an integer, return it as-is
-        if isinstance(obj, int):
-            if self.pass_through is False:
-                raise ValueError(f"Mixed types detected for {self.name}: {obj}")
-            self.pass_through = True
-            return obj
+        if not self.force_identify:
+            if isinstance(obj, int):
+                if self.pass_through is False:
+                    raise ValueError(f"Mixed types detected for {self.name}: {obj}")
+                self.pass_through = True
+                return obj
+            elif isinstance(obj, np.integer):
+                if self.pass_through is False:
+                    raise ValueError(f"Mixed types detected for {self.name}: {obj}")
+                self.pass_through = True
+                return int(obj) # Convert numpy integer to Python integer
 
         if self.pass_through is True:
             raise ValueError(f"Mixed types detected for {self.name}: {obj}")
@@ -37,10 +55,15 @@ class Identifier:
 
     def get_id(self, obj: Any) -> Optional[int]:
         # If the object is an integer, return it as-is
-        if isinstance(obj, int):
-            if not self.pass_through:
-                raise ValueError(f"Mixed types detected for {self.name}: {obj}")
-            return obj
+        if not self.force_identify:
+            if isinstance(obj, int):
+                if not self.pass_through:
+                    raise ValueError(f"Mixed types detected for {self.name}: {obj}")
+                return obj
+            elif isinstance(obj, np.integer):
+                if not self.pass_through:
+                    raise ValueError(f"Mixed types detected for {self.name}: {obj}")
+                return int(obj)
 
         return self.obj_to_id.get(obj, None)
 
